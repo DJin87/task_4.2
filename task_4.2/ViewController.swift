3a30b8adcb27f62a6f7c99897aae91ef3b6d8a6c
DJin87 <jins@inbox.lv>
2020-12-12 20:20:07 +0200

    Initial Commit

---
+++ task_4.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task_4.2
+//
+//  Created by DJin on 12/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
